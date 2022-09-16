359b6b7bcfc57e2412b92e0473e4784a74864a5a
nthduc <nguyenthaiduc0212@gmail.com>
2022-09-16 18:20:25 +0700

    fix GlobalStyle type Node -> JSX.Element

--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -1,14 +1,12 @@
 import { PropsWithChildren } from 'react';
 import './GlobalStyles.scss';
 
-interface Props extends PropsWithChildren<unknown>{   }
+interface Props extends PropsWithChildren<unknown>{  
+    children: JSX.Element;
+ }
 // Remove React.FC from Typescript template
 const GlobalStyles  = ({ children }: Props) : JSX.Element => {
-    return(
-        <>
-     {children}
-        </>
-    )
+    return children;
 
 };
 
