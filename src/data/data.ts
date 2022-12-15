ad0356cec99cfeb1b11165711fe7c20a03ac645f
nthduc <nguyenthaiduc0212@gmail.com>
2022-12-16 00:21:21 +0700

    :white_check_mark: Live Page

---
+++ src/data/data.ts
@@ -0,0 +1,54 @@
+import { images } from '@/assets/images';
+const Data = [
+    {
+        id: 1,
+        name: 'JuJo Bin',
+        background: images.coin,
+        avatar: images.coin,
+        content: 'Thôi...không xong rồiiii haizzzz Thôi...không xong rồiiii',
+        music: 'nhạc nền - Beautiful in White',
+        viewers: 904,
+    },
+    {
+        id: 2,
+        name: 'Hoang y Nhi',
+        background: images.check,
+        avatar: images.check,
+        content: 'em có hai nụ cười🍑🍑🍑',
+        viewers: 750,
+    },
+    {
+        id: 3,
+        name: 'Thy Thy',
+        avatar: images.userGroup,
+        background: images.userGroup,
+        content: 'Cuốn z là p đu ngay💃🏻',
+        viewers: 129,
+    },
+    {
+        id: 4,
+        background: images.feedback,
+        avatar: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/8ff7a8e134012d4c36ea2d6a0cc8d90c~c5_100x100.jpeg?x-expires=1660615200&x-signature=Pvg0fbTz2Bk4O82EeU1XOdiNNwk%3D',
+        name: '๖ۣۜҪôηɠ•Ҫɦúล ๖ۣۜHòลηɠ•Ŧửツ',
+        content: 'em đâu làm gì sai đâu sao bỏ em vậy😞',
+        viewers: 1223,
+    },
+    {
+        id: 5,
+        name: 'Tiểu Hý',
+        background: images.keyboard,
+        avatar: images.keyboard,
+        content: 'Cái ngày mà tui còn mập nè🤣',
+        viewers: 4958,
+    },
+    {
+        id: 6,
+        name: 'fan sam tổng mãi đỉnh 😝😝',
+        background: images.language,
+        avatar: images.language,
+        content: 'hello mn',
+        viewers: 879,
+    },
+];
+
+export default Data;
---
+++ src/pages/Live/LiveVideoItem.tsx
@@ -0,0 +1,54 @@
+import classNames from 'classnames/bind';
+import { BsPlayCircle } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import Image from '@/components/Image';
+import styles from './VideoItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+    avatar: any;
+    nickName?: string;
+    id: number;
+    name: string;
+    background: string;
+    content: string;
+    viewers: string | number;
+
+}
+    // Remove React.FC from Typescript template
+const LiveVideoItem = ({ avatar, nickName, id, name, background, content, viewers }: Props) => {
+    return (
+        <div className={cx('wrapper')}>
+            <div
+                className={cx('video-image')}
+                style={{
+                    backgroundImage: 'url(' + background + ')',
+                }}
+            >
+                <div className={cx('footer')}>
+                    <div className={cx('audience')}>
+                        <p className={cx('audience-count')}>{viewers}</p>
+                        <p className={cx('audience-text')}>viewers</p>
+                    </div>
+                    <div className={cx('live-label')}>LIVE</div>
+                </div>
+                <div className={cx('play-hover')}>
+                    <BsPlayCircle className={cx('icon-play')} />
+                </div>
+            </div>
+
+            <div className={cx('author-video')}>
+                <Link className={cx('image-body')} to={{ pathname: '/@' + nickName, search: 'id=' + id }}>
+                    <Image src={avatar} className={cx('image')} />
+                </Link>
+                <div className={cx('body')}>
+                    <p className={cx('content')}>{content}</p>
+                    <p className={cx('name')}>{name}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LiveVideoItem;
