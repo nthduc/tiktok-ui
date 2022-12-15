d6a0c981390aac3d11bafcd06baac1345c09295f
nthduc <nguyenthaiduc0212@gmail.com>
2022-12-16 00:02:54 +0700

    :white_check_mark: Following Page

--- src/pages/Following/Following.tsx
+++ src/pages/Following/Following.tsx
@@ -1,29 +1,67 @@
-import React from 'react';
-import Portal from '@/components/Portal';
+import classNames from 'classnames/bind';
+import FollowingVideo from '@/components/FollowingVideo';
+import styles from './Following.module.scss';
 
-const Following: React.FC = () => {
+import * as userService from '@/services/userService';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import LoadingIcon from '@/assets/LoadingIcon';
+import { DataUserSuggested } from '@/interfaces';
 
-  const handlePublish = () => {
+const INIT_PAGE = 1;
+const PER_PAGE = 5;
 
-  }
+const cx = classNames.bind(styles);
 
-  return (
-    <div>
-      <h1>Following</h1>
-      <Portal containerId='publish-btn'>
-        <button onClick={handlePublish}>Save and Publish</button>
-      </Portal>
-    </div>
-  )
-}
+const Following: React.FC = (): JSX.Element => {
+    // useState
+    const [suggestedUser, setSuggestedUser] = useState<Array<DataUserSuggested>>([]);
+    const [page, setPage] = useState<number>(INIT_PAGE);
+    const [more, setMore] = useState<boolean>(false);
+
+    const observer = useRef() as React.MutableRefObject<IntersectionObserver>;
+    const [loading, setLoading] = useState<boolean>(true);
+
+    // useEffect
+    useEffect(() => {
+        userService
+            .getSuggested({ page, perPage: PER_PAGE })
+            .then((data) => {
+                setSuggestedUser((prev) => [...prev, ...data]);
+                setMore(data.length > 0);
+                setLoading(false);
+            })
+            .catch((error) => console.log(error));
+        // }
+    }, [page]);
+    // console.log(followUser);
+
+    // useCallBack
+    const lastRef = useCallback((node: Element) => {
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                setPage((prevPage) => prevPage + 1);
+            }
+        });
+        if (node) observer.current.observe(node);
+    }, []);
+
+    return loading ? (
+        <div className={cx('loading')}>
+            <LoadingIcon />
+        </div>
+    ) : (
+        <FollowingVideo
+            accountList={suggestedUser}
+            // @ts-ignore
+            lastVideoRef={lastRef}
+        />
+    );
+};
 
 export default Following;
 
-
-
-
-
 /**
-  * @author - Nguyen Thai Duc
-  * @course -  F8
-*/
+ * @author - Nguyen Thai Duc
+ * @course -  F8
+ */
